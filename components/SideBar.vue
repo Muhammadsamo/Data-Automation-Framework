cee5ca6d59f052245adc270473a67dbb606176d3
Muhammad Samo <bldstrikr@gmail.com>
2023-07-15 16:24:48 +0500

    added basic sidebar

---
+++ components/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="h-screen fixed left-0 bg-[#F1F1F1] w-[313px] transition-all ease"
+    :class="{ '-translate-x-[100%]': !sidebarOpen }"
+  >
+    <span
+      class="absolute right-[-16px] top-[50px] bg-[#F1F1F1] w-[16px] h-[40px] rounded-r-[5px] rounded-br-[5px] flex cursor-pointer"
+      @click="toggleSidebar"
+    >
+      <img
+        src="/sidebar/sidebar-toggle.svg"
+        width="8"
+        height="13px"
+        alt="sidebar toggle button"
+        :class="{ 'rotate-180': !sidebarOpen }"
+      />
+    </span>
+  </div>
+</template>
+
+<script setup>
+const sidebarOpen = ref(true);
+
+const toggleSidebar = () => {
+  sidebarOpen.value = !sidebarOpen.value;
+};
+</script>
--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,7 +1,8 @@
 <template>
   <main>
-    <h1 class="text-3xl font-bold underline text-green-400 mt-2 text-center">
-     Projecs Page
+    <SideBar />
+    <h1 class="text-3xl font-bold underline text-green-400 text-center">
+      Projecs Page
     </h1>
   </main>
 </template>
