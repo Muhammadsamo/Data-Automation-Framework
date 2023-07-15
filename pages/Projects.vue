8eb13f995618103ba4ea2bf5b18327d8a1da12b7
Muhammad Samo <bldstrikr@gmail.com>
2023-07-15 21:16:03 +0500

    updated sidebar

--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,8 +1,29 @@
 <template>
   <main>
-    <SideBar />
+    <SideBar title="Projects" :tabs="tabs" />
     <h1 class="text-3xl font-bold underline text-green-400 text-center">
       Projecs Page
     </h1>
   </main>
 </template>
+
+<script setup>
+const tabs = [
+  {
+    tabId: 1,
+    tabTitle: "BR CoF Reporting",
+  },
+  {
+    tabId: 2,
+    tabTitle: "DEF D Reporting",
+  },
+  {
+    tabId: 3,
+    tabTitle: "ABC Loan FX Conversion",
+  },
+  {
+    tabId: 4,
+    tabTitle: "LR1555 Adjustments",
+  },
+];
+</script>
