4538a58b0ded171f852d33ba9ab7fd4d0525d270
Muhammad Samo <bldstrikr@gmail.com>
2023-07-15 08:16:06 +0500

    edited nuxt.config

--- .netlify/functions-internal/server/chunks/app/styles.mjs
+++ .netlify/functions-internal/server/chunks/app/styles.mjs
@@ -1,6 +1,6 @@
 const interopDefault = r => r.default || r || [];
 const styles = {
-  "node_modules/.pnpm/nuxt@3.6.2_@types+node@18.16.19/node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.96608170.mjs').then(interopDefault),
+  "node_modules/.pnpm/nuxt@3.6.2_@types+node@18.16.19/node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.5e0aa3cf.mjs').then(interopDefault),
   "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.49960bed.mjs').then(interopDefault),
   "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.7a0a864e.mjs').then(interopDefault)
 };
