67ff4ed870eeb3e4c3035945f0e280dada48f8a6
muhammadshumas <shumaslaghari@gmail.com>
2023-07-17 15:35:53 +0500

    updated table component

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,7 @@
     <h1 class="text-3xl font-bold underline text-green-400 text-center">
       Home Page
     </h1>
-    <!-- <button @click="show = true">Show Modal</button> -->
-    <!-- <UIModal v-model:show="show" heading="New Task" /> -->
-    <TableDataTable
+    <!-- <TableDataTable
       heading="Datasets"
       :columns="columns"
       :rows="rows"
@@ -22,51 +20,51 @@
           {{ value }}
         </span>
       </template>
-    </TableDataTable>
+    </TableDataTable> -->
   </main>
 </template>
 
 <script setup lang="ts">
-const columns = [
-  {
-    label: "Name",
-    field: "name",
-    sortable: true,
-    tdClass: "underline",
-  },
-  {
-    label: "File Name",
-    field: "fileName",
-    sortable: true,
-  },
-  {
-    label: "Provider",
-    field: "provider",
-    sortable: true,
-  },
-  {
-    label: "Issues",
-    field: "issues",
-    sortable: true,
-    thClass: "text-center",
-  },
-];
+// const columns = [
+//   {
+//     label: "Name",
+//     field: "name",
+//     sortable: true,
+//     tdClass: "underline",
+//   },
+//   {
+//     label: "File Name",
+//     field: "fileName",
+//     sortable: true,
+//   },
+//   {
+//     label: "Provider",
+//     field: "provider",
+//     sortable: true,
+//   },
+//   {
+//     label: "Issues",
+//     field: "issues",
+//     sortable: true,
+//     thClass: "text-center",
+//   },
+// ];
 
-const rows = [
-  {
-    name: "MarketReturns",
-    fileName: "FinancialStatements_{YYYY}-{MM}.txt",
-    provider: "Bloomberg",
-    issues: 1,
-  },
-  {
-    name: "FinancialStatements",
-    fileName: "MarketReturns_{YYYY}-{MM}.xlsx",
-    provider: "Brookfield",
-    meta: {
-      disableDetails: true,
-    },
-  },
-];
+// const rows = [
+//   {
+//     name: "MarketReturns",
+//     fileName: "FinancialStatements_{YYYY}-{MM}.txt",
+//     provider: "Bloomberg",
+//     issues: 1,
+//   },
+//   {
+//     name: "FinancialStatements",
+//     fileName: "MarketReturns_{YYYY}-{MM}.xlsx",
+//     provider: "Brookfield",
+//     meta: {
+//       disableDetails: true,
+//     },
+//   },
+// ];
 // const show = ref(false);
 </script>
